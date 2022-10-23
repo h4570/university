b41bf42ff8254c2a1db0d76118e36df25db4acd3
h4570 <sandro.sobczynski@gmail.com>
2022-10-23 10:06:32 +0200

    add excercise 2

---
+++ 7-semester-tau/excercise-2/src/main.js
@@ -0,0 +1,13 @@
+const validateEmail = (email) => {
+    if (email === undefined || email === null || email === '') {
+        throw new Error('Please provide an email address');
+    }
+
+    return String(email)
+        .toLowerCase()
+        .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
+};
+
+module.exports = validateEmail;
